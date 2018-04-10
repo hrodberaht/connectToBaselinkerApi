4c76626d9ff6a0541db1dd7f8b57b91b5ae1dd78
hrodberaht <robpszczolka@gmail.com>
2018-04-10 21:24:00 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace nauka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            GetData();
+
+            Console.ReadLine();
+            
+        }
+
+        static async void GetData()
+        {
+            string b = "\"order_id\"";
+            var client = new HttpClient();
+
+            var data = new FormUrlEncodedContent(new List<KeyValuePair<string,string>>{
+                new KeyValuePair<string, string>("token", Settings.Token),
+                new KeyValuePair<string, string>("method", "getOrders"),
+                new KeyValuePair<string, string>("parameters", "{" + $"{b}: 34090396"+"}"),
+            } );
+
+            HttpResponseMessage a = await client.PostAsync("https://api.baselinker.com/connector.php",data);
+            Console.WriteLine(a.Content.ReadAsStringAsync().Result);
+            Console.WriteLine($"{b}: 34090396 ");
+            
+
+        }
+    }
+}
